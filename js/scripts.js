3d41507a2342fb940217734ec3910f5c9db9ce8f
mohammed ateeq ur rehman <ateeq.13.rehman@gmail.com>
2020-10-18 15:27:18 +0530

    NPM scripts
    there is a small hunch, we have to download the parallelshell ver.3.0.1 so that the error doesn't show up.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 2000
+    });
+    $('#carouselButton').click(function () {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')){
+            $('#myCarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#myCarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+});
